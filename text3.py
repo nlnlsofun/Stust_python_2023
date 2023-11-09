0e5c86626aed52399471ecb2d611b0de642bb1ce
nlnlsofun <117150985+nlnlsofun@users.noreply.github.com>
2023-11-09 11:41:41 +0800

    Stust_python_2023

---
+++ text3.py
@@ -0,0 +1,9 @@
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+
+    return addition, subtraction
+
+
+res = calculation(40, 10)
+print(res)
---
+++ text4.py
@@ -0,0 +1,5 @@
+def show_employee(name, salary=9000):
+    print("名子:", name, "薪水:", salary)
+
+show_employee("Ben", 12000)
+show_employee("Jessa")
