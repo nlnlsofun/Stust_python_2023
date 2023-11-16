c2bbc5a8e455da2c41aba348daf7eceb707aa96e
nlnlsofun <117150985+nlnlsofun@users.noreply.github.com>
2023-11-16 12:02:59 +0800

    class20231116

---
+++ class_python_2023-11-16/work-1.py
@@ -0,0 +1,18 @@
+class Myshape:
+  def __int__(self,side,length,width,radius,spheric):
+    self.side=side
+    self.length=length
+    self.width=width
+    self.radius=radius
+    self.spheric=spheric 
+    spheric= radius*radius
+  def round(a):
+    print("圓面積"+a.spheric)
+
+p1 = Myshape(2,3,6,4,5)
+print(round)
+
+
+ 
+
+
